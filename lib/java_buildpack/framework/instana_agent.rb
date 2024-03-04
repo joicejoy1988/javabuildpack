38a1759ddb3b9f34ed5381a28590c74fa955f154
Joice Joy <joicejoy@Joices-MacBook-Pro.local>
2024-03-04 19:13:18 +0530

    download_uri corrected

--- lib/java_buildpack/framework/instana_agent.rb
+++ lib/java_buildpack/framework/instana_agent.rb
@@ -67,7 +67,7 @@ module JavaBuildpack
         #download_uri = "https://_:#{credentials[AGENT_KEY]}@artifact-public.instana.io/artifactory/rel-generic-instana-virtual/com/instana/standalone-collector-jvm/1.264.1/standalone-collector-jvm-1.264.1.jar"
         #download_uri = 'https://imagestorage04.blob.core.windows.net/pub/standalone-collector-jvm-1.264.1.jar'
         download_uri = "https://_:#{credentials[AGENT_KEY]}@artifact-public.instana.io/artifactory/rel-generic-instana-virtual/com/instana/standalone-collector-jvm/[RELEASE]/standalone-collector-jvm-[RELEASE].jar"
-        #['latest', download_uri]
+        ['latest', download_uri]
       end
 
       def setup_variables
