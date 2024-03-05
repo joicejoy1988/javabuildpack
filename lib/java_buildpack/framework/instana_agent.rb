4ef2a67b89c3a4498ece8414d58ce6d02f0e2f35
Joice Joy <joicejoy@Joices-MacBook-Pro.local>
2024-03-05 10:34:33 +0530

    ruby edit

--- lib/java_buildpack/framework/instana_agent.rb
+++ lib/java_buildpack/framework/instana_agent.rb
@@ -62,12 +62,11 @@ module JavaBuildpack
       ENDPOINT_URL = 'endpointurl'
       INSTANA_AGENT_KEY = 'INSTANA_AGENT_KEY'
       INSTANA_ENDPOINT_URL = 'INSTANA_ENDPOINT_URL'
-      RELEASE = 'RELEASE'
 
       def standalone_agent_download_url
         #download_uri = "https://_:#{credentials[AGENT_KEY]}@artifact-public.instana.io/artifactory/rel-generic-instana-virtual/com/instana/standalone-collector-jvm/1.264.1/standalone-collector-jvm-1.264.1.jar"
         #download_uri = 'https://imagestorage04.blob.core.windows.net/pub/standalone-collector-jvm-1.264.1.jar'
-        download_uri = "https://_:#{credentials[AGENT_KEY]}@artifact-public.instana.io/artifactory/rel-generic-instana-virtual/com/instana/standalone-collector-jvm/#{[RELEASE]}/standalone-collector-jvm-#{[RELEASE]}.jar"
+        download_uri = "https://_:#{credentials[AGENT_KEY]}@artifact-public.instana.io/artifactory/rel-generic-instana-virtual/com/instana/standalone-collector-jvm//\[RELEASE\]//standalone-collector-jvm-/\[RELEASE\]/.jar"
         ['latest', download_uri]
       end
 
